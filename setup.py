aebc992e1d85341a7958e3f4f9fac7b2b0b44e60
RG TQ UG <flora@national.shitposting.agency>
2016-02-15 13:11:40 -0600

    also updated the url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Rg Tq Ug',
   author_email = 'rosecross@openmailbox.org',
   url = 'https://github.com/RgTqUg/lamia', 
-  download_url = 'https://github.com/RgTqUg/Lamia/archive/0.1.tar.gz', 
+  download_url = 'https://github.com/RgTqUg/Lamia/archive/0.11.tar.gz', 
   keywords = ['api', 'danbooru', 'wrapper'],
   classifiers = [],
 )
