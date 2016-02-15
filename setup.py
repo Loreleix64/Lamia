ce32e82237aba167709f66294c531ad43ac640e0
RG TQ UG <flora@national.shitposting.agency>
2016-02-15 13:09:58 -0600

    updated release version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'lamia',
   packages = ['lamia'], 
-  version = '0.1',
+  version = '0.11',
   description = 'API wrapper for Danbooru',
   author = 'Rg Tq Ug',
   author_email = 'rosecross@openmailbox.org',
