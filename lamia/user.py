386758856b6a21e62d5b5583b75b8fbff523e774
RG TQ UG <flora@national.shitposting.agency>
2016-02-18 18:21:14 -0600

    Update user.py

--- lamia/user.py
+++ lamia/user.py
@@ -33,26 +33,28 @@ class api(object):
 			auth=HTTPBasicAuth(self.user, self.key), files=files)
 	
 	def get_dmails(self):
-		dmails = []
+		mails = []
 		mailbox = requests.get("http://danbooru.donmai.us/dmails.json", auth=HTTPBasicAuth(self.user, self.key))
-		for mail in mailbox:
-			msg = dmails.dmail(msg, self)
-			dmails.append(msg)
+		dmailbox = mailbox.json()
+		for mail in dmailbox:
+			msg = dmails.dmail(mail, self)
+			mails.append(msg)
 			
-		return dmails
+		return mails
 	
 	def send_dmail(self, title, body, to):
-		requests.post("http//danbooru.donmai.us/dmails.json?dmail[to_name]=" + to + "&dmail[title]=" + title + "&dmail[body]=" + body, auth=HTTPBasicAuth(self.user, self.key))
+		requests.post("http://danbooru.donmai.us/dmails.json?dmail[to_name]=" + to + "&dmail[title]=" + title + "&dmail[body]=" + body, auth=HTTPBasicAuth(self.user, self.key))
 		
 	def create_entry(self, title, body):
 		requests.post(("http://danbooru.donmai.us/wiki_pages.json?wiki_page[title]=" + str(title) + "&wiki_page[body]=" + str(body)),
 					 auth=HTTPBasicAuth(self.user, self.key))
 		
 	def get_entries(self, terms):
-		search = requests.get(("http//danbooru.donmai.us/wiki_pages.jsonn?search[title]=" str(terms)), auth=HTTPBasicAuth(self.user, self.key))
 		results = []
+		search = requests.get(("http://danbooru.donmai.us/wiki_pages.json?search[title]=" + terms), auth=HTTPBasicAuth(self.user, self.key))
 		table = search.json()
 		for entries in table:
 			entry = wiki.entry(entries, self)
 			results.append(entry)
 		
+		return results
