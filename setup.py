2e5735969d302735c1b3f4ac20986f9e9723f49a
RG TQ UG <flora@national.shitposting.agency>
2016-02-18 18:21:58 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'lamia',
   packages = ['lamia'], 
-  version = '0.11',
+  version = '0.2',
   description = 'API wrapper for Danbooru',
   author = 'Rg Tq Ug',
   author_email = 'rosecross@openmailbox.org',
   url = 'https://github.com/RgTqUg/lamia', 
-  download_url = 'https://github.com/RgTqUg/Lamia/archive/0.11.tar.gz', 
+  download_url = 'https://github.com/RgTqUg/Lamia/archive/0.2.tar.gz', 
   keywords = ['api', 'danbooru', 'wrapper'],
   classifiers = [],
 )
